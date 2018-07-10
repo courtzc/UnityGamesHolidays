e524e46c1d8124e1c06e5b4bea1346f345e78090
courtzc <32031285+courtzc@users.noreply.github.com>
2018-07-10 12:43:03 +1000

    End scene added, moving platform animated

--- Assets/Scripts/GameLogic/PlatformController.cs
+++ Assets/Scripts/GameLogic/PlatformController.cs
@@ -5,16 +5,6 @@ using UnityEngine;
 
 public class PlatformController : MonoBehaviour
 {
-    [Header("The farthest left the platform can move")]
-    public float distanceLeft;
-    [Header("The farthest right the platform can move")]
-    public float distanceRight;
-    [Header("The farthest up the platform can move")]
-    public float distanceUp;
-    [Header("The farthest down the platform can move")]
-    public float distanceDown;
-
-
     public void Start()
     {
 
@@ -22,43 +12,23 @@ public class PlatformController : MonoBehaviour
 
     public void Update()
     {
-        PlatformMove();
+
     }
 
     public void OnTriggerEnter2D(Collider2D coll)
     {
-        if (coll.transform.name == "Player")
+        if (coll.transform.name == "MovingPlatform")
         {
-            Debug.Log("I'm colliding");
-            coll.gameObject.GetComponent<Rigidbody2D>().isKinematic = true;
-            GetComponent<Rigidbody2D>().isKinematic = true;
-            coll.transform.parent = transform;
-
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                coll.gameObject.GetComponent<Rigidbody2D>().isKinematic = false;
-            }
+            transform.parent = coll.transform;
         }
     }
 
 
     public void OnTriggerExit2D(Collider2D coll)
     {
-        Debug.Log("I'm no longer colliding");
-        coll.gameObject.GetComponent<Rigidbody2D>().isKinematic = false;
-        GetComponent<Rigidbody2D>().isKinematic = false;
-
-        Debug.Log("I have detached and made people orphans");
-        transform.DetachChildren();
-    }
-
-
-
-
-
-    public void PlatformMove()
-    {
-        transform.position = new Vector3(Mathf.Clamp(transform.position.x, distanceLeft, distanceRight),
-            Mathf.Clamp(transform.position.y, distanceUp, distanceDown), transform.position.z);
+        if (coll.transform.name == "MovingPlatform")
+        {
+            coll.transform.DetachChildren();
+        }
     }
 }
