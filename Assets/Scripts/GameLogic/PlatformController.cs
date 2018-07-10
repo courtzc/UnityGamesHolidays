43a3bcfcf3d4c0a4880e34bbecf71019c612a156
AB <annabelle.macfarlane@gmail.com>
2018-07-10 10:07:00 +1000

    pulled

---
+++ Assets/Scripts/GameLogic/PlatformController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+
+public class PlatformController : MonoBehaviour
+{
+    [Header("The farthest left the platform can move")]
+    public float distanceLeft;
+    [Header("The farthest right the platform can move")]
+    public float distanceRight;
+    [Header("The farthest up the platform can move")]
+    public float distanceUp;
+    [Header("The farthest down the platform can move")]
+    public float distanceDown;
+
+
+    public void Start()
+    {
+        
+    }
+
+    public void Update()
+    {
+        
+    }
+
+    public void OnTriggerStay2D(Collider2D collision)
+    {
+        
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        
+    }
+
+}
